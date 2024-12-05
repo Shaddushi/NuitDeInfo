a4295179eb3e708576f8a4d8fcbdb559c654dffe
td1711 <darquestom@gmail.com>
2024-12-05 22:52:33 +0100

    update

--- Vue-frontEnd/src/App.vue
+++ Vue-frontEnd/src/App.vue
@@ -3,15 +3,7 @@ import { ref, onMounted } from 'vue';
 import Home from './pages/Home/Home.vue'
 import Header from './partials/Header.vue'
 import axios from 'axios';
-
-<<<<<<< HEAD
-const myModal = ref(null)
-
-=======
->>>>>>> ae05812c926fca9e937d434f1abba757862ffe6a
-
 </script>
-
 
 <template>
     <div class="h-100">
@@ -19,4 +11,3 @@ const myModal = ref(null)
         <Home />
     </div>
 </template>
-
---
+++ Vue-frontEnd/src/components/ElementModal.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+    buttonClass: {
+        type: String,
+        default: "btn btn-primary"
+    }
+})
+import Modal from "@/components/Modal.vue";
+</script>
+
+<template>
+    <modal :button-class="'btn btn-outline-secondary border-secondary border rounded-circle '+buttonClass">
+        <template #button>
+            <i class="bi bi-search"></i>
+        </template>
+    </modal>
+</template>
