6b825a39c0f152ca3ba077bcaf8b7cdff80be007
B4PT12T3 <125573072+B4PT12T3@users.noreply.github.com>
2024-12-06 06:37:25 +0100

    uuuu

--- Vue-frontEnd/src/partials/Header.vue
+++ Vue-frontEnd/src/partials/Header.vue
@@ -12,19 +12,23 @@
 
 
 <template>
-        <header class="bg-secondary py-4">
+        <header class="bgcoLor bg-secondary py-4">
             <div class="container d-flex justify-content-between">
                 <div>
-                    <a href="/" class="text-white text-decoration-none title-font">SEaXfriends</a>
+                    <a href="/" >
+                        <img src="../assets/logo.png" class="logo">
+                    </a>
                 </div>
 
                 <div>
-                    <nav id="nav-bar">
-                        <RouterLink to="/">Home</RouterLink>
-                        <RouterLink to="/credits">Credits</RouterLink>
-                        <RouterLink to="/succes">Succes</RouterLink>
+                    <nav id="nav-bar" class="center">
+                        <div v-if="name">
+                        Bonjour  {{ name }}
+                    </div>
+                        <RouterLink class="e" to="/">Home</RouterLink>
+                        <RouterLink class="e"  to="/succes">Succes</RouterLink>
                     </nav>
-                    {{ name }}
+              
                 </div>
 
             </div>
