87916ca36b3087fe5d74fdc0a956e7992abf907e
HugoFOLLET <70712434+Newball875@users.noreply.github.com>
2024-12-06 01:18:59 +0100

    Captcha fonctionnel

--- Vue-frontEnd/src/App.vue
+++ Vue-frontEnd/src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { RouterLink } from 'vue-router'
 import Home from './pages/Home/Home.vue'
 import Header from './partials/Header.vue'
 import axios from 'axios';
 
-<<<<<<< HEAD
 const myModal = ref(null)
 
-=======
->>>>>>> ae05812c926fca9e937d434f1abba757862ffe6a
 
 </script>
 
@@ -16,7 +14,9 @@ const myModal = ref(null)
 <template>
     <div class="h-100">
         <Header />
-        <Home />
+		<RouterView></RouterView>
     </div>
+	<div>
+	</div>
 </template>
 
